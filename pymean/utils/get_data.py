467dfc0964a2f82859226c6887aa40b244a6cddf
KeironO <keion.oshea@gmail.com>
2019-04-17 18:40:54 +0100

    Hmm

--- pymean/utils/get_data.py
+++ pymean/utils/get_data.py
@@ -7,24 +7,21 @@ def _check_dotdir(dotfile_dir):
     if not os.path.isdir(dotfile_dir):
         os.makedirs(dotfile_dir)
 
-def _check_if_exists(dotfile_dir, database):
-    print(os.path.join(dotfile_dir, database+".json"))
-    if not os.path.join(dotfile_dir, database+".json"):
-        return False
-    else:
-        return True
+def _check_if_exists(fp):
+    return os.path.isfile(fp)
+
 
 def _download(dotfile_dir, database):
     r = request.urlopen(DATA_URL + database+".json").read()
     print("Hello World")
 
-def get_data(database="kegg"):
+def get_data(database="kegg", species="hsa"):
     home_dir = os.path.expanduser("~")
     dotfile_dir = os.path.join(home_dir, ".pymean/")
     _check_dotdir(dotfile_dir)
-    exists = _check_if_exists(dotfile_dir, database)
-    print(exists)
-    if exists:
+    fp = os.path.join(dotfile_dir, "%s_%s_pathways.json" % (database, species))
+    exists = _check_if_exists(fp)
+    if _check_if_exists(fp):
         pass
     else:
         _download(dotfile_dir, database)
