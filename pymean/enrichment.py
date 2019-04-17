9997f3e54b74330649268aadcbb7e6393c727070
KeironO <keion.oshea@gmail.com>
2019-04-17 19:20:21 +0100

    Need to clean this up fairly badly

--- pymean/enrichment.py
+++ pymean/enrichment.py
@@ -3,25 +3,26 @@ from scipy.stats import binom_test
 import json
 import pandas as pd
 import os
-from .utils.get_data import get_data
+from utils.get_data import get_data
 
 class EnrichmentAnalysis:
-    def __init__(self, compound_list, type="compounds", database="kegg", organism="hsa"):
+    def __init__(self, compound_list, database="kegg", organism="hsa"):
         self.compound_list = compound_list
-        self.type = type
+        self.database = database
         self.organism = organism
-
         self.pathway_data = self._load_data()
+
 
     def _load_data(self):
         return get_data(self.database, self.organism)
 
-    def run_analysis(self, pvalue_cutoff=0.05, p=0, alternative="two-sided", adj_method="bonferroni"):
+    def run_analysis(self, pvalue_cutoff=0.05, alternative="two-sided", adj_method="bonferroni"):
         results = []
-        for pathway in self.pathway_data:
-            pathway_compounds = self.pathway_data[pathway][self.type]
-            pathway_name = self.pathway_data[pathway]["name"]
-            in_pathway = [x for x in self.clusters if x in pathway_compounds]
+        p = self.pathway_data["population"]
+        for pathway in self.pathway_data["pathways"]:
+            pathway_compounds = self.pathway_data["pathways"][pathway]["compounds"]
+            pathway_name = self.pathway_data["pathways"][pathway]["name"]
+            in_pathway = [x for x in self.compound_list if x in pathway_compounds]
             p_value = binom_test(len(in_pathway), len(pathway_compounds), 1/p, alternative)
             results.append([pathway, pathway_name, "(%i / %i)" % (len(in_pathway), len(pathway_compounds)), p_value, "; ".join(in_pathway)])
 
@@ -37,3 +38,17 @@ class EnrichmentAnalysis:
         results.sort_values("adj. p-value", inplace=True)
 
         return results
+
+if __name__ == "__main__":
+    ea = EnrichmentAnalysis([
+        "C00022",
+        "C00024",
+        "C00148",
+        "C00036",
+        "C00065",
+        "C00049",
+        "C00631",
+        "C00152"
+    ])
+
+    print(ea.run_analysis(pvalue_cutoff=0.05))
