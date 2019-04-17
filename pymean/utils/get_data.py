f23d952a8c7f415f56a77bd987859457d8ffa7f8
KeironO <keion.oshea@gmail.com>
2019-04-17 18:55:11 +0100

    Added more

--- pymean/utils/get_data.py
+++ pymean/utils/get_data.py
@@ -1,7 +1,26 @@
 import os
 from urllib import request
+import json
 
 DATA_URL = "http://users.aber.ac.uk/keo7/pymean/"
+PATHWAYS_FILE = "%s_%s_pathways.json"
+TIMESTAMP_FILE = "%s_%s_timestamp.json"
+
+def _load_data(dotfile_dir, pathways_file):
+    with open(os.path.join(dotfile_dir, pathways_file), "r") as infile:
+        return json.load(infile)
+
+def _check_if_old(dotfile_dir, timestamp_file):
+    timestamp = json.loads(request.urlopen(DATA_URL + timestamp_file).read())
+
+    with open(os.path.join(dotfile_dir, timestamp_file), "r") as infile:
+        our_timestamp = json.load(infile)
+
+    if timestamp["version"] > our_timestamp["version"]:
+        return True
+    else:
+        return False
+
 
 def _check_dotdir(dotfile_dir):
     if not os.path.isdir(dotfile_dir):
@@ -11,22 +30,31 @@ def _check_if_exists(fp):
     return os.path.isfile(fp)
 
 
-def _download(dotfile_dir, database):
-    r = request.urlopen(DATA_URL + database+".json").read()
-    print("Hello World")
+def _download(dotfile_dir, pathways_file, timestamp_file):
+    r = request.urlopen(DATA_URL + pathways_file).read()
+    r = json.loads(r)
+    with open(os.path.join(dotfile_dir, pathways_file), "w") as outfile:
+        json.dump(r, outfile, indent=4)
+
+    r = request.urlopen(DATA_URL + timestamp_file).read()
+    r = json.loads(r)
+
+    with open(os.path.join(dotfile_dir, timestamp_file), "w") as outfile:
+        json.dump(r, outfile, indent=4)
+
 
 def get_data(database="kegg", species="hsa"):
     home_dir = os.path.expanduser("~")
     dotfile_dir = os.path.join(home_dir, ".pymean/")
     _check_dotdir(dotfile_dir)
-    fp = os.path.join(dotfile_dir, "%s_%s_pathways.json" % (database, species))
+    pathways_file = PATHWAYS_FILE % (database, species)
+    timestamp_file = TIMESTAMP_FILE % (database, species)
+    fp = os.path.join(dotfile_dir, pathways_file)
     exists = _check_if_exists(fp)
     if _check_if_exists(fp):
-        pass
+        if _check_if_old(dotfile_dir, timestamp_file):
+            _download(dotfile_dir, pathways_file, timestamp_file)
     else:
-        _download(dotfile_dir, database)
+        _download(dotfile_dir, pathways_file, timestamp_file)
 
-
-
-if __name__ == "__main__":
-    get_data()
+    return _load_data(dotfile_dir, pathways_file)
